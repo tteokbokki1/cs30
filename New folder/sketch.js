05ca5a70525bd803be944931ce1c2ef6d5a84643
tteokbokki1 <sungjaden603@gmail.com>
2025-09-29 09:12:49 -0600

    Update sketch.js

--- New folder/sketch.js
+++ New folder/sketch.js
@@ -1,28 +1,35 @@
-let time = 2000;
-let lastSwitch = 0;
-let isPink = false;
+//Interactive scene
+//Jaden Sung
+
+
+
+//let myArray2 = ['black','white', 'black','white', 'black','white', 'black','white']
+//let myArray = ['white', 'black'];
 
 function setup() {
-  createCanvas(400, 400);
+  if (windowWidth > windowHeight) {
+    createCanvas(windowHeight, windowHeight);
+  }
+  if (windowHeight > windowWidth) {
+    createCanvas(windowWidth, windowWidth);
+  }
+  else{
+    createCanvas(windowWidth, windowHeight);
+  }
 }
 
 function draw() {
-  theChange();
-  switchBackground();
-}
-
-
-function switchBackground(){
-  if (isPink){
-    background('pink')
+  let squareSize = windowWidth / 8;
+  background(220);
+  for (let x = 0; x < 8; x ++) {
+    for (let y = 0; y < 8; y ++) {
+      if ((x + y) % 2 === 0) {
+        fill(255);
+      } else {
+        fill(0);
+      }
+      rect(x * squareSize,y * squareSize, squareSize, squareSize);
+    }
   }
-  else {
-    background('black')
-  }
 }
 
-function theChange(){
-  if (millis() > lastSwitch + time)
-    isPink = !isPink;
-    lastSwitch = millis();
-}
