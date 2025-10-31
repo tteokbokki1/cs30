333602372dfbaaf489c38a04a05c7dddb1151e45
tteokbokki1 <sungjaden603@gmail.com>
2025-10-31 09:44:31 -0600

    qwe oiy21984y98wdf

--- gridg/sketch.js
+++ gridg/sketch.js
@@ -1,15 +1,24 @@
-// Project Title
-// Your Name
-// Date
+// The legend of zelda
+// Jaden Sung
+// 10/31/20205
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
+frames = {
+  run: []
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
+function preload(){
+  for (let i = 1; i <= totalrun; i++){
+    frames.run.push(loadImage('gridg/Link frames/Basic-movement/basic-frame_run_${i}.png'));
+  }
+}
 
 function draw() {
   background(220);
+  image()
+  
 }
--- moving/sketch.js
+++ moving/sketch.js
@@ -1,19 +1,28 @@
 //Neighbors demo(part of grid demo)
 
+const PLAYER = 4;
+const WALL = 1;
+const OPEN_TILE = 0;
 const CELL_SIZE = 50;
 let grid;
 let rows;
 let cols;
+let thePlayer = {
+  x: 0,
+  y: 0,
+};
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth * 0.9, windowHeight * 0.9);
   cols = Math.floor(width/CELL_SIZE);
   rows = Math.floor(height/CELL_SIZE);
   grid = generateRandomGrid(cols, rows);
+
+  grid[thePlayer.y][thePlayer.x] = PLAYER;
 }
 
 function draw() {
-  background(220);
+  background("aqua");
   displayGrid();
 }
 
@@ -24,21 +33,21 @@ function mousePressed() {
   //self
   toggleCell(x ,y);
 
-  //neighbours
-  toggleCell(x + 1, y);
-  toggleCell(x - 1, y);
-  toggleCell(x, y - 1);
-  toggleCell(x, y + 1);
+  // //neighbours
+  // toggleCell(x + 1, y);
+  // toggleCell(x - 1, y);
+  // toggleCell(x, y - 1);
+  // toggleCell(x, y + 1);
 }
 
 function toggleCell(x, y) {
   //make sure the cell you're toggling actually exists!
-  if (x >= 0 && x < cols && y >= 0 && y < rows) {
-    if (grid[y][x] === 0) {
+  if (x >= OPEN_TILE && x < cols && y >= OPEN_TILE && y < rows) {
+    if (grid[y][x] === OPEN_TILE) {
       grid[y][x] = 1;
     }
     else if (grid[y][x] === 1) {
-      grid[y][x] = 0;
+      grid[y][x] = OPEN_TILE;
     }
   }
 }
@@ -50,16 +59,31 @@ function keyPressed() {
   else if (key === "e") {
     grid = generateEmptyGrid(cols, rows);
   }
+  else if (key === "w") {
+    movePlayer(thePlayer.x, thePlayer.y - 1);
+  }
+  else if (key === "s") {
+    movePlayer(thePlayer.x, thePlayer.y + 1);
+  }
+  else if (key === "d") {
+    movePlayer(thePlayer.x + 1, thePlayer.y);
+  }
+  else if (key === "a") {
+    movePlayer(thePlayer.x - 1, thePlayer.y);
+  }
 }
 
 function displayGrid() {
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
-      if (grid[y][x] === 0) {
+      if (grid[y][x] === OPEN_TILE) {
         fill("white");
       }
       else if (grid[y][x] === 1) {
         fill("black");
+      }
+      else if (grid[y][x] === PLAYER) {
+        fill("red");
       }
       square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
     }
@@ -73,7 +97,7 @@ function generateRandomGrid(cols, rows) {
     for (let x = 0; x < cols; x++) {
       //pick 0 or 1 randomly
       if (random(100) < 50) {
-        newGrid[y].push(0);
+        newGrid[y].push(OPEN_TILE);
       }
       else {
         newGrid[y].push(1);
@@ -88,8 +112,22 @@ function generateEmptyGrid(cols, rows) {
   for (let y = 0; y < rows; y++) {
     newGrid.push([]);
     for (let x = 0; x < cols; x++) {
-      newGrid[y].push(0);
+      newGrid[y].push(OPEN_TILE);
     }
   }
   return newGrid;
+}
+
+function movePlayer(x, y) {
+  if (x  >= 0 && x < cols && y >= 0 && y < rows && grid[y][x] === OPEN_TILE){
+    let oldX = thePlayer.x;
+    let oldY = thePlayer.y;
+
+
+    thePlayer.x = x;
+    thePlayer.y = y;
+    grid[thePlayer.y][thePlayer.x] = PLAYER;
+
+    grid[oldY][oldX] = OPEN_TILE;
+  }
 }
